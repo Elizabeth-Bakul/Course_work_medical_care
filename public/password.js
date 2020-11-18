01b8d89551e830d8efa3996577171dfdf66bcb7a
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-18 19:49:45 +0300

    2kgfdg

---
+++ public/password.js
@@ -0,0 +1,9 @@
+let showPassword = document.querySelector('.show-password');
+showPassword.onchange = function () {
+    if (showPassword.checked) {
+      password.type = 'text';
+    } else {
+      password.type = 'password';
+    }
+  };
+  
