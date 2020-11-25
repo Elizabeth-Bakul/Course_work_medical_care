92094f17739fe0d0c25d2df195eb406afbcfa092
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-25 16:28:31 +0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports={
 
     ensureAuthenticated: function(req, res, next) {
         if( req.isAuthenticated()) { return next();}
-        req.flash('danger', 'req.isAuthenticated()');
+        req.flash('warning', req.isAuthenticated());
         req.flash('danger', 'Please log in');
     res.redirect('/login');
     },
