f90ebbfff16a208df960b072ff3e9fc82dcf8628
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-25 16:04:43 +0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports={
         if( req.isAuthenticated()) { return next()}
         req.flash('danger', 'Please log in');
     res.redirect('/routes/login');
-    }
+    },
     forwardAuthenticated: function(req, res, next) {
         if(!req.isAuthenticated()){ return next()}
         res.redirect('/account');
