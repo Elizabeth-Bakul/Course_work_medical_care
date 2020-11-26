69cf346c9f001d91a8c2873243a6b935483bfb20
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-27 00:12:36 +0300

    hhh

--- public/data.js
+++ public/data.js
@@ -1,7 +1,9 @@
-var now = new Date().toLocaleString(); 
+ 
 let but4 = document.querySelector('.button_3');
 let tim=document.querySelector('.date-time');
 
 but4.onclick=()=>{
+    var now = new Date().toLocaleString();
+    console.log(now);
     tim.value=now;
 }
