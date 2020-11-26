238d6fa60a556537da93ffaefbb364bdc8efd642
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-27 00:33:27 +0300

    ggg

--- public/data.js
+++ public/data.js
@@ -3,7 +3,23 @@ let but4 = document.querySelector('.button_3');
 let tim=document.querySelector('.date-time');
 
 but4.onclick=()=>{
-    var now = new Date().toLocaleString();
+    tim.removeAttribute('readonly');
+    var now = new Date()
+    var year=now.getFullYear();
+    var month=now.getMonth();
+    var day=now.getDate();
+    var hour=now.getHours();
+    var minut=now.getMinutes()
     console.log(now);
-    tim.value=now;
+    console.log(year);
+    console.log(month);
+    console.log(day);
+    console.log(hour);
+    console.log(minut);
+    if (month<10){
+        month='0'+month;
+    } 
+    console.log(month);
+    var now2=new Date().toISOString;
+    tim.value=now2;
 }
