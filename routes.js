6a15f3c83c8dfef2b06631d0602ee10f223421a1
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-28 20:30:23 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -136,7 +136,9 @@ module.exports = function (app) {
                     else {
                         //client.query('COMMIT')
                         console.log( 'Страховая компания добавлена.')
-                        client.query('select id from "Insurance" where "InsuranceName"=$1',[req.body.str], function (err2, result2){result.rows[0].id=result2.rows[0].id;})
+                        client.query('select id from "Insurance" where "InsuranceName"=$1',[req.body.str], function (err2, result2){
+                            console.log(result2.rows[0].id);
+                            result.rows[0].id=result2.rows[0].id;})
                     }
                 })
                 }
