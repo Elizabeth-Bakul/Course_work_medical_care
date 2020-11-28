fecaaf77240234887b9c3296367aa61ae324b959
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-29 01:03:09 +0300

    Update data.js

--- public/data.js
+++ public/data.js
@@ -4,7 +4,7 @@ let tim=document.querySelector('.date-time');
 function getISOStringWithoutSecsAndMillisecs1(date) {
     const dateAndTime = date.toISOString().split('T')
     const time = dateAndTime[1].split(':')
-    
+    const sec=time[2].split('.')
     return dateAndTime[0]+'T'+time[0]+':'+time[1]+':'+time[2]
   }
   
