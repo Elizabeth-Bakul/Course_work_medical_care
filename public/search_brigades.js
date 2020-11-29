8d8e0a3a81839eb13e657481d2d7e7302dc66fca
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-29 16:43:34 +0300

    mjhbnkb

---
+++ public/search_brigades.js
@@ -0,0 +1,10 @@
+var buttons=document.querySelectorAll('.button2');
+var forms=document.getElementsByTagName("form");
+for (let i=0; i<buttons.length;i++){
+    buttons[i].addEventListener("click", fucntion(){buttonsControl(this,i)},false);
+}
+function buttonsControl(button, i){
+    console.log(i)
+    console.log(button.value);
+    console.log(forms[i].className);
+}
