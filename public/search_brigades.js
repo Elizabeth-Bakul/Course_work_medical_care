22f2032b23355ab691580648fc1d846436105ce4
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-29 17:15:45 +0300

    jknj

--- public/search_brigades.js
+++ public/search_brigades.js
@@ -1,6 +1,6 @@
 var buttons=document.querySelectorAll('.button2');
 var forms=document.getElementsByTagName("form");
-//var inp=document.getElementsByTagName("input");
+var inp=document.getElementsByTagName("input");
 for (let i=0; i<buttons.length;i++){
     buttons[i].addEventListener("click", function(e){
         e.preventDefault();
@@ -11,7 +11,7 @@ function buttonsControl(button, i){
     console.log(button.value);
     let brigades=JSON.stringify(
         {
-        idBrigades:button.value
+        idBrigades:inp[i].value
     }
     );
     let request = new XMLHttpRequest();
@@ -23,7 +23,7 @@ function buttonsControl(button, i){
     request.send(brigades);
     request.addEventListener("load", function () {
         let receivedBrigades = JSON.parse(request.response);
-        
+
     })
     console.log(forms[i].className);
 }
