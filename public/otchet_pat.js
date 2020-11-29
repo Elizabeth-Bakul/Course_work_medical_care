ddd0af53b4ccba77c314b88b8a27463a56186b11
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-30 01:01:45 +0300

    оооо

--- public/otchet_pat.js
+++ public/otchet_pat.js
@@ -3,4 +3,16 @@ let sec=document.querySelector('.js-selectize')
 but.addEventListener("click", function (e){
     e.preventDefault();
     console.log(sec.value);
+    let pat=JSON.stringify(
+        {
+        idPat:sec.value
+    })
+    let request = new XMLHttpRequest();
+    request.open("POST", "/search_patient", true); // посылаем запрос на адрес "/user"
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function () {
+        console.log(this.response);
+    };
+    request.send(patient);
+    
 })
