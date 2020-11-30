41f265fb0d1ed2abe216a9f400257f52d80d8df8
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-30 23:25:58 +0300

    dsf

--- public/data.js
+++ public/data.js
@@ -15,14 +15,19 @@ but3.onclick=()=>{
     tim.setAttribute('readonly', 'true');
 }
 
-let but31 = document.querySelector('.Big_B');
+let but31 = document.querySelectorAll('.Big_B');
 let tim2=document.querySelector('.date-time2');
 let req=document.querySelector('.rrrrreq');
 let brig=document.querySelector('.brrrr');
-but31.onclick=()=>{
+for (let i=0; i<buttons.length;i++){
+  but31[i].addEventListener("click", function(e){
+      //e.preventDefault();
+      buttonsControl(this,i)},false);
+}
+function buttonsControl(button, i){
   tim2.value=getISOStringWithoutSecsAndMillisecs1(new Date());
-  console.log(but31.value);
-  req.value=but31.value;
+  console.log(button[i].value);
+  req.value=button[i].value;
   console.log(req.value);
   console.log(tim2.value);
   console.log(brig.value);
