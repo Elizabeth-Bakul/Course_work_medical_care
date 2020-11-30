0ead17ce1e2ccff4296b597fa37d2792686ab8c2
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-11-30 19:28:31 +0300

    aa

---
+++ public/payroll_calculation.js
@@ -0,0 +1,13 @@
+let button=document.getElementById('r_z')
+
+let dat=document.getElementById('dat-zp')
+let n=document.querySelector('.n')
+let id=document.querySelector('.id')
+let typ=document.querySelector('.typ')
+let zp=document.querySelector('.zp_res')
+
+button.addEventListener("click", function (e){
+    e.preventDefault();
+    console.log(dat.value)
+    
+})
