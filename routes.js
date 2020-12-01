65a936f19aae6208407771d03391161230b20e3f
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-12-01 19:45:50 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -135,7 +135,59 @@ module.exports = function (app) {
     app.post('/account_doctor',ensureAuthenticated, async function (req, res){
         try{
             console.log(req.body);
-            
+            const client = await pool.connect()
+            await client.query('BEGIN')
+            await JSON.stringify(client.query('UPDATE public."Requests" SET  "AcceptTime"=$1, "Brigade_id_fk"=$2 WHERE id=$3',[req.body.date,req.body.brigade, req.body.id_req],function(err,result){
+                if(err){res.flash('danger','Ошибка с обновлением данных');
+                        res.redirect('/account_doctor')}
+                else {
+                    client.query('COMMIT');
+                    console.log('Update success');
+                    return;
+                }
+            }))
+            client.release()
+            const client2 = await pool.connect()
+            await client2.query('BEGIN')
+            await JSON.stringify(client2.query('select "Requests".id, "InformalDescription","RequestTime","AcceptTime", "PatientSurname", "PatientName","PatientMiddleName","PatientAddress" from "Requests" left join "Patients" on "Requests"."Patient_fk"="Patients".id where "Requests".id=$1',[req.body.id_req], function(err1, result1){
+                if(err1){console.log(err1)
+                    req.flash('danger', "Ошибка с поиском Вызовов")
+                res.redirect('/account_doctor')
+                        }
+                else {
+                    console.log(result1.rows);
+                    client2.query('select id, "Symptom_name" from "Symptoms"',[],function(err2,result2){
+                        if(err2){console.log(err2)
+                            req.flash('danger', "Ошибка с поиском Симптомов")
+                            res.redirect('/account_doctor')}
+                        else{
+                            console.log(result2.rows)
+                            client2.query('select id, "AnalysisName" from "Analysis"',[],function(err3,result3){
+                                if(err3){console.log(err3)
+                                    req.flash('danger', "Ошибка с поиском Исследования")
+                                res.redirect('/account_doctor')}
+                                else{
+                                    console.log(result3.rows)
+                                    res.send('account_doc_karta',{
+                                        userData: req.user,
+                                        ReqData: result1.rows,
+                                        SymData:result2.rows,
+                                        IsData:result3.rows,
+                                        messages: {
+                                            danger: req.flash('danger'),
+                                            warning: req.flash('warning'),
+                                            success: req.flash('success')
+                                        }
+                                        
+                                    })
+                                    client2.query('COMMIT')
+                                    }})
+                                }
+                            })
+                        }
+                    })
+            )
+            client2.release()
         }
         catch (e) {
             throw(e)
