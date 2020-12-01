00221dba36c8c6124b2177e672eb0e8f1b3d3ad2
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-12-01 22:20:37 +0300

    hhhh

--- public/zap_cart.js
+++ public/zap_cart.js
@@ -3,6 +3,12 @@ var f1=document.querySelector('.vyzov');
 var brigad=document.querySelectorAll('.brrrr');
 var surname2=document.querySelector('.surname');
 var f2=document.querySelector('.form_req');
+var name2=document.querySelector('.name');
+var lastname2=document.querySelector('.Lastname');
+var address2=document.querySelector('.adress');
+var ops2=document.querySelector('.ops');
+var dateRequest2=document.querySelector('.dateRequest');
+var dateAccept2=document.querySelector('.dateAccept');
 
 console.log(but31);
 for (let i=0; i<but31.length;i++){
@@ -43,6 +49,16 @@ let request = new XMLHttpRequest();
       surname2.setAttribute('readonly','true');
       name2.value=receivedRequest.ReqData[0].PatientName;
       name2.setAttribute('readonly','true');
+      lastname2.value=receivedRequest.ReqData[0].PatientMiddleName;
+      lastname2.setAttribute('readonly','true')
+      address2.value=receivedRequest.ReqData[0].PatientAddress;
+      address2.setAttribute('readonly','true')
+      ops2.value=receivedRequest.ReqData[0].InformalDescription;
+      ops2.setAttribute('readonly','true')
+      dateRequest2.value=receivedRequest.ReqData[0].RequestTime;
+      dateRequest2.setAttribute('readonly','true')
+      dateAccept2.value=receivedRequest.ReqData[0].AcceptTime;
+      dateAccept2.setAttribute('readonly','true')
     });
 
   //tim2[i].value=;
