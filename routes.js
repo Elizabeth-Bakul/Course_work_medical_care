f91a0be0b0ba495870c24ea55b30a18b622676b1
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2020-12-04 04:19:48 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1039,7 +1039,7 @@ module.exports = function (app) {
                                     flag: 'false2'
                                 })
                             } else {
-                                client.query('select id from "Diagnosis-Medicines" where "Diagnosis_id_fk"=$2 and "Medicines_id_fk"=$1', [result1[0].id,result2[0].id], function(err3,result3){
+                                client.query('select id from "Diagnosis-Medicines" where "Diagnosis_id_fk"=$2 and "Medicines_id_fk"=$1', [result1.rows[0].id, result2.rows[0].id], function(err3,result3){
                                     if(err3){console.log(err3)}
                                     else {
                                         if (result3.rowCount===0){
@@ -1048,7 +1048,7 @@ module.exports = function (app) {
                                             })
                                         }
                                         else {
-                                            client.query('delete from "Diagnosis-Medicines" where "Diagnosis_id_fk"=$1 and "Medicines_id_fk"=$2',[result1[0].id,result2[0].id],function (err,result){
+                                            client.query('delete from "Diagnosis-Medicines" where "Diagnosis_id_fk"=$1 and "Medicines_id_fk"=$2',[result1.rows[0].id,result2.rows[0].id],function (err,result){
                                     if (err){
                                         console.log(err)
                                         }
@@ -1205,7 +1205,7 @@ module.exports = function (app) {
                                     flag: 'false2'
                                 })
                             } else {
-                                client.query('select id from "Diagnosis-Medicines" where "Diagnosis_id_fk"=$1 and "Medicines_id_fk"=$2', [result1[0].id,result2[0].id], function(err3,result3){
+                                client.query('select id from "Diagnosis-Medicines" where "Diagnosis_id_fk"=$1 and "Medicines_id_fk"=$2', [result1.rows[0].id,result2.rows[0].id], function(err3,result3){
                                     if(err3){console.log(err3)}
                                     else {
                                         if (result3.rowCount!=0){
@@ -1214,7 +1214,7 @@ module.exports = function (app) {
                                             })
                                         }
                                         else {
-                                            client.query('INSERT INTO "Diagnosis-Medicines" ("Diagnosis_id_fk", "Medicines_id_fk") VALUES($1,$2)',[result1[0].id,result2[0].id],function (err,result){
+                                            client.query('INSERT INTO "Diagnosis-Medicines" ("Diagnosis_id_fk", "Medicines_id_fk") VALUES($1,$2)',[result1.rows[0].id,result2.rows[0].id],function (err,result){
                                     if (err){
                                         console.log(err)
                                         }
